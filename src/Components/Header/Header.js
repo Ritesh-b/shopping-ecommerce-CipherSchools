45c2c8bd9cd801643d6ac1bc75b5f72ff97ca5c7
Ritesh-b <84612870+Ritesh-b@users.noreply.github.com>
2022-07-18 20:53:17 +0530

    final commit...

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <nav className="header">
+      <h1 style={{ color: "white" }}>Shopping Site</h1>
+      <div>
+        <a href="/shop">Shop</a>
+        <a href="/orders">Orders</a>
+        <a href="/inventory">Inventory</a>
+        <a href="/about">About</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
